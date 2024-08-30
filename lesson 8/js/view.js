98d52e0e1956a7d032d96a23bf4a105b7793e2b3
Alex Sivets <localhost83@gmail.com>
2024-08-30 16:53:20 +0300

    Add lesson 8

---
+++ lesson 8/js/view.js
@@ -0,0 +1,36 @@
+const contactList = {
+  // рендер на чистому JS
+  // render(contacts, element) {
+  //   contacts.forEach(contact => {
+  //     let divContact = document.createElement('div');
+  //     divContact.classList.add('contact');
+
+  //     divContact.insertAdjacentHTML(
+  //       'beforeend',
+  //       `<h2>${contact.firstName} ${contact.lastName}</h2>`
+  //     );
+  //     divContact.insertAdjacentHTML(
+  //       'beforeend',
+  //       `<p>Phone: ${contact.phone}</p>`
+  //     );
+  //     divContact.insertAdjacentHTML(
+  //       'beforeend',
+  //       `<p>Email: ${contact.email}</p>`
+  //     );
+  //     divContact.insertAdjacentHTML(
+  //       'beforeend',
+  //       `<p>${contact.description}</p>`
+  //     );
+
+  //     element.appendChild(divContact);
+  //   });
+  // },
+
+  renderWithTemplate(contacts, element, template) {
+    let html = '';
+    contacts.forEach(contact => {
+      html += Mustache.render(template, contact);
+    });
+    element.innerHTML = html;
+  },
+};
